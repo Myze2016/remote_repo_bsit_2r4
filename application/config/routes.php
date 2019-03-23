6dacaabd32c79c8cfe112d48eccde971a91065de
Sudo Man <sudoman@yahoo.com>
2019-03-22 20:27:49 -0700

    test

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
-| -------------------------------------------------------------------------
+|qwkjehwqeh -------------------------------------------------------------------------
 | URI ROUTING
 | -------------------------------------------------------------------------
 | This file lets you re-map URI requests to specific controller functions.
